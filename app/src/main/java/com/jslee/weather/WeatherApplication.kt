ff5980526bd526931e03d014a48afc51d717a84c
JaesungLeee <viki0114@naver.com>
2023-05-15 17:02:30 +0900

    [chore] #1 hilt setup

--- app/src/main/java/com/jslee/weather/WeatherApplication.kt
+++ app/src/main/java/com/jslee/weather/WeatherApplication.kt
@@ -1,11 +1,13 @@
 package com.jslee.weather
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * WeatherApp
  * @author jaesung
  * @created 2023/05/15
  */
+@HiltAndroidApp
 class WeatherApplication : Application() {
 }
